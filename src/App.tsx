da174fcbe67464e0aade1bae349a1f2e10bbf81e
kobakichi <fenderstrat57vin@gmail.com>
2022-05-20 04:52:30 +0900

    commponent make

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Home } from "./components/Home/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
